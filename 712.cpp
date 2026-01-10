abec468b0ace6a34b8a2d4919611300a5de725d8
Ansh kathil <kathilansh@gmail.com>
2026-01-10 12:59:48 +0530

    problem 712

---
+++ 712.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int solve(string &s1, string& s2 ,int i ,int j ){
+        if(i==s1.size()){
+            int sum = 0 ;
+            for(int k = j ; k < s2.size();k++)
+                sum += s2[k];
+            return sum;
+        }
+
+        if(j==s2.size()){
+            int sum = 0 ;
+            for(int k = i ; k < s1.size();k++)
+                sum += s1[k];
+            return sum;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        if(s1[i] == s2[j]){
+            return dp[i][j] = solve(s1,s2,i+1,j+1);
+        }
+
+        int del_s1 = s1[i] + solve(s1,s2,i+1,j);
+        int del_s2 = s2[j] + solve(s1,s2,i,j+1);
+
+        return dp[i][j] = min(del_s1,del_s2);
+    }
+
+    int minimumDeleteSum(string s1, string s2) {
+        memset(dp,-1,sizeof(dp));
+        return solve(s1 , s2 ,0,0);
+    }
+};
